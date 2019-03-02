6329f6e5a6b85d64fa2543fd26a50b7acc396907
Hunter Carlisle <huntingcarlisle@gmail.com>
2019-03-02 05:58:40 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,17 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class YouTubeDebugger extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      
+    }
+  }
+  
+  
+  render() {
+    return null;
+  }
+}
