0edf2dc59a37ce0d7fe8d1b5f24fd639bb744a88
Hunter Carlisle <huntingcarlisle@gmail.com>
2019-03-02 05:59:24 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -6,7 +6,13 @@ export default class YouTubeDebugger extends React.Component {
   constructor() {
     super();
     this.state = {
-      
+        js { 
+          errors: [], 
+          user: null, 
+          settings: { 
+            bitrate: 8, 
+            video: { 
+              resolution: '1080p' } } }
     }
   }
   
