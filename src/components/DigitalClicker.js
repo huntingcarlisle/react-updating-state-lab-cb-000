1c10c6d1dc4d365ac49ee69414d529a4df0a60f2
Hunter Carlisle <huntingcarlisle@gmail.com>
2019-03-02 05:49:15 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,9 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class DigitalClicker extends React.Component {
+  render() {
+    return null;
+  }
+}
