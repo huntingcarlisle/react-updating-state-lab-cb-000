d1c5f726e7aa4b1b64db3990d55eff5158a0c656
Hunter Carlisle <huntingcarlisle@gmail.com>
2019-03-02 05:55:32 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -11,7 +11,9 @@ export default class DigitalClicker extends React.Component {
   }
   
   handleClick = () => {
-    this.setState()
+    this.setState({
+      timesClicked += 1
+    })
   }
   
   render() {
